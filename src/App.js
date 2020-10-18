0497fd552ba73bbfacd29cb81c1f53a14e9fa5fd
Ciaran Kilkenny <g00334394@gmit.ie>
2020-10-18 23:31:09 +0100

    Lab 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Header } from './components/header'; //import  
+import { Footer } from './components/footer'; //all
+import { Content } from './components/content';//components
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component { //Changed function to classes 
+  render() {
+    return (
+      <div className="App">
+
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link> 
+            <Nav.Link href="/read">Read</Nav.Link>
+            <Nav.Link href="/create">Create</Nav.Link>
+          </Nav>
+        </Navbar>
+
+      
+        <Header></Header>
+        <h1>Hello World!</h1>
+        <Content></Content>
+        <Footer></Footer>
+      </div>
+    );
+  }
 }
 
 export default App;
